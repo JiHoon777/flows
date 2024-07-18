7c11bef84aa9cf3d4d1cc881f2c4fa838845cebc
JiHoon7773 <thdwlgnsl@naver.com>
2024-07-19 01:44:21 +0900

    flow-node > 자식 카드들 표시

--- src/components/flow-node/flow-node.tsx
+++ src/components/flow-node/flow-node.tsx
@@ -2,10 +2,40 @@ import { observer } from 'mobx-react'
 import { NodeProps } from 'reactflow'
 
 import { NodeWrap } from '@/components/flow-node/node-wrap'
+import { For } from '@/components/for.tsx'
+import { Card, CardHeader, CardTitle } from '@/components/ui/card.tsx'
 import { FlowNodeData } from '@/store/types'
+import { useStore } from '@/store/useStore.ts'
 
 export const FlowNode = observer(
   (props: NodeProps<FlowNodeData> & { type: 'flow' }) => {
-    return <NodeWrap {...props}></NodeWrap>
+    const { id, type } = props
+    const store = useStore()
+    const flow = store.flowStore.getFlowById(id)
+
+    return (
+      <NodeWrap {...props}>
+        <div className={'w-full flex flex-col gap-2'}>
+          <p className={'text-left w-full text-xs px-2 font-extralight'}>
+            Cards: {(flow.childNodeIds ?? []).length}, Flow:{' '}
+            {(flow.childFlowIds ?? []).length}
+          </p>
+          <div className={'w-full h-full flex flex-wrap gap-4'}>
+            <For
+              each={flow.childNodeIds ?? []}
+              map={(nodeId) => store.nodeStore.getNodeById(nodeId)}
+            >
+              {(node) => (
+                <Card key={node.id} className={'w-[10rem]'}>
+                  <CardHeader>
+                    <CardTitle>{node.title}</CardTitle>
+                  </CardHeader>
+                </Card>
+              )}
+            </For>
+          </div>
+        </div>
+      </NodeWrap>
+    )
   },
 )
