daa6f037996d203bdbbffe17d24058e5b362c788
JiHoon7773 <thdwlgnsl@naver.com>
2024-07-19 01:44:09 +0900

    refactor : for

--- src/components/for.tsx
+++ src/components/for.tsx
@@ -1,15 +1,25 @@
 import { ReactNode } from 'react'
 
-type Props<T> = {
+type MapFunction<T, MT> = (item: T) => MT
+
+type Props<T, MT = T> = {
   each: T[]
-  children: (item: T, index: number) => ReactNode
+  map?: MapFunction<T, MT>
+  children: (item: MT extends T ? T : MT, index: number) => ReactNode
 }
 
-export const For = <T,>({ each, children }: Props<T>) => {
+export const For = <T, MT = T>({ each, map, children }: Props<T, MT>) => {
   return (
     <>
       {each.map((item, index) => {
-        return children(item, index)
+        if (map) {
+          const mappedItem = map(item)
+          return mappedItem
+            ? children(mappedItem as MT extends T ? T : MT, index)
+            : null
+        }
+
+        return children(item as MT extends T ? T : MT, index)
       })}
     </>
   )
