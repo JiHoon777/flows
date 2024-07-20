00a86743f15d7bfa1273675c00e10f6edc102968
JiHoon7773 <thdwlgnsl@naver.com>
2024-07-20 16:29:00 +0900

    refactor: flow-node

--- src/components/flow-node/flow-node.tsx
+++ src/components/flow-node/flow-node.tsx
@@ -6,6 +6,14 @@ import { For } from '@/components/for.tsx'
 import { Card, CardHeader, CardTitle } from '@/components/ui/card.tsx'
 import { useStore } from '@/store/useStore.ts'
 import { FlowNodeData } from '@/types/flow.type.ts'
+import { cn } from '@/utils/cn.ts'
+
+const cns = {
+  container: cn('flex w-full flex-col gap-2'),
+  desc: cn('w-full px-2 text-left text-xs font-extralight'),
+  content: cn('flex h-full w-full flex-wrap gap-4'),
+  card: cn('w-[10rem]'),
+}
 
 export const FlowNode = observer((props: NodeProps<FlowNodeData>) => {
   const { id } = props
@@ -14,18 +22,18 @@ export const FlowNode = observer((props: NodeProps<FlowNodeData>) => {
 
   return (
     <NodeWrap {...props} type={'flow'}>
-      <div className={'flex w-full flex-col gap-2'}>
-        <p className={'w-full px-2 text-left text-xs font-extralight'}>
+      <div className={cns.container}>
+        <p className={cns.desc}>
           Cards: {(flow.childNodeIds ?? []).length}, Flow:{' '}
           {(flow.childFlowIds ?? []).length}
         </p>
-        <div className={'flex h-full w-full flex-wrap gap-4'}>
+        <div className={cns.content}>
           <For
             each={flow.childNodeIds ?? []}
             map={(nodeId) => store.nodeStore.getNodeById(nodeId)}
           >
             {(node) => (
-              <Card key={node.id} className={'w-[10rem]'}>
+              <Card key={node.id} className={cns.card}>
                 <CardHeader>
                   <CardTitle>{node.title}</CardTitle>
                 </CardHeader>
