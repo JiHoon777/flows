aae5f47f58ee0139cc292da5ab1a426eb7d3222c
JiHoon7773 <thdwlgnsl@naver.com>
2024-08-03 18:28:21 +0900

    editor-state

--- src/lib/lexical/lexical-editor-state.ts
+++ src/lib/lexical/lexical-editor-state.ts
@@ -1,13 +1,18 @@
-import type { SerializedRootNode } from './nodes/lexical-root-node'
-import { $createRootNode } from './nodes/lexical-root-node'
 import type { LexicalEditor } from '@/lib/lexical/lexical-editor.ts'
 import type {
+  LexicalNode,
   NodeMap,
   SerializedLexicalNode,
 } from '@/lib/lexical/lexical-node.ts'
 import type { BaseSelection } from '@/lib/lexical/lexical-selection.ts'
+import type { SerializedElementNode } from '@/lib/lexical/nodes/lexical-element-node.ts'
+import type { SerializedRootNode } from '@/lib/lexical/nodes/lexical-root-node.ts'
 
 import { readEditorState } from '@/lib/lexical/lexical-updates.ts'
+import { $getRoot } from '@/lib/lexical/lexical-utils.ts'
+import { $isElementNode } from '@/lib/lexical/nodes/lexical-element-node.ts'
+import { $createRootNode } from '@/lib/lexical/nodes/lexical-root-node.ts'
+import invariant from '@/utils/invariant.ts'
 
 export interface SerializedEditorState<
   T extends SerializedLexicalNode = SerializedLexicalNode,
@@ -25,17 +30,21 @@ export class EditorState {
   _flushSync: boolean
   _readOnly: boolean
 
-  constructor(nodeMap: NodeMap) {
+  constructor(nodeMap: NodeMap, selection?: null | BaseSelection) {
     this._nodeMap = nodeMap
-    this._selection = null
+    this._selection = selection || null
     this._flushSync = false
     this._readOnly = false
   }
 
+  /**
+   * 에디터 상태가 비어 있는지 확인합니다.
+   *
+   * @returns 노드 맵에 루트 노드만 존재하고 선택 영역이 없는 경우 true를 반환합니다. 그렇지 않으면 false를 반환합니다.
+   */
   isEmpty(): boolean {
-    return false
+    return this._nodeMap.size === 1 && this._selection === null
   }
-
   /**
    * 에디터 상태를 읽는 작업을 실행합니다. 이 함수는 에디터 컨텍스트를 사용할 수 있게 하며
    * (내보내기 및 읽기 전용 DOM 작업에 유용합니다) 에디터 상태의 변이를 방지합니다.
@@ -51,6 +60,31 @@ export class EditorState {
       this,
       callbackFn,
     )
+  }
+  /**
+   * 현재 에디터 상태의 복제본을 생성합니다.
+   *
+   * @param selection - 선택적으로 설정할 선택 영역입니다. null일 수도 있습니다.
+   * @returns 복제된 EditorState 객체를 반환합니다.
+   */
+  clone(selection?: null | BaseSelection): EditorState {
+    const editorState = new EditorState(
+      this._nodeMap,
+      selection === undefined ? this._selection : selection,
+    )
+    editorState._readOnly = true
+
+    return editorState
+  }
+  /**
+   * 현재 에디터 상태를 직렬화된 JSON 형식으로 변환합니다.
+   *
+   * @returns 직렬화된 EditorState 객체를 반환합니다.
+   */
+  toJSON(): SerializedEditorState {
+    return readEditorState(null, this, () => ({
+      root: exportNodeToJSON($getRoot()),
+    }))
   }
 }
 
@@ -81,6 +115,54 @@ export function editorStateHasDirtySelection(
   return false
 }
 
+export function cloneEditorState(current: EditorState): EditorState {
+  return new EditorState(new Map(current._nodeMap))
+}
+
 export function createEmptyEditorState(): EditorState {
   return new EditorState(new Map([['root', $createRootNode()]]))
+}
+
+/**
+ * LexicalNode를 직렬화된 JSON 형식으로 내보냅니다.
+ *
+ * @param node - 직렬화할 LexicalNode입니다.
+ * @returns 직렬화된 노드를 반환합니다.
+ */
+function exportNodeToJSON<SerializedNode extends SerializedLexicalNode>(
+  node: LexicalNode,
+): SerializedNode {
+  const serializedNode = node.exportJSON()
+  const nodeClass = node.constructor
+
+  if (serializedNode.type !== nodeClass.getType()) {
+    invariant(
+      false,
+      'LexicalNode: Node %s does not match the serialized type. Check if .exportJSON() is implemented and it is returning the correct type.',
+      nodeClass.name,
+    )
+  }
+
+  if ($isElementNode(node)) {
+    const serializedChildren = (serializedNode as SerializedElementNode)
+      .children
+    if (!Array.isArray(serializedChildren)) {
+      invariant(
+        false,
+        'LexicalNode: Node %s is an element but .exportJSON() does not have a children array.',
+        nodeClass.name,
+      )
+    }
+
+    const children = node.getChildren()
+
+    for (let i = 0; i < children.length; i++) {
+      const child = children[i]
+      const serializedChildNode = exportNodeToJSON(child)
+      serializedChildren.push(serializedChildNode)
+    }
+  }
+
+  // @ts-expect-error
+  return serializedNode
 }
