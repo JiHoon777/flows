1fbe3f97a0942cf55c7a4730e97cc40af3264514
JiHoon7773 <thdwlgnsl@naver.com>
2024-08-09 19:16:20 +0900

    tauri : traffic lights inset

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,20 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{LogicalPosition, Manager};
+use tauri_plugin_trafficlights_positioner::WindowExt;
+
 fn main() {
   tauri::Builder::default()
+    .setup(move |app| {
+      if let Some(window) = app.get_window("main") {
+        #[cfg(target_os = "macos")]
+        // NOTE: Make sure you only call this ONCE per window.
+        let _ = window.setup_traffic_lights_inset(LogicalPosition::new(20.0, 20.0));
+      };
+
+      Ok(())
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
