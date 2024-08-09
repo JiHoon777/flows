5beadde5f22d6ac91c39bc2fa51d569e5e889205
JiHoon7773 <thdwlgnsl@naver.com>
2024-08-09 20:52:26 +0900

    app routes 분리

--- src/app-routes.tsx
+++ src/app-routes.tsx
@@ -1,66 +1,17 @@
-import type { ComponentType } from 'react'
-
 import { observer } from 'mobx-react'
-import { lazy as _lazy, Suspense } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Suspense } from 'react'
 
 import { RegisterGlobalHotkeys } from '@/components/hotkey/RegisterGlobalHotkeys.tsx'
 import { SiteLayout } from '@/components/site/siteLayout.tsx'
-
-type LazyComponentModule = { [key: string]: ComponentType<any> }
-
-function lazy<T extends LazyComponentModule, K extends keyof T>(
-  importFunc: () => Promise<T>,
-  componentName: K,
-) {
-  return _lazy(() =>
-    importFunc().then((module) => ({
-      default: module[componentName],
-    })),
-  )
-}
-
-const FlowDetailView = lazy(
-  () => import('@/views/flow-detail-view'),
-  'FlowDetailViewParamsWrap',
-)
-const NodeDetailView = lazy(
-  () => import('@/views/node-detail-view'),
-  'NodeDetailViewParamsWrap',
-)
-const HomeView = lazy(() => import('@/views/homeView'), 'HomeView')
-const FlowsView = lazy(() => import('@/views/flowsView'), 'FlowsView')
-const DocumentsView = lazy(
-  () => import('@/views/documentsView'),
-  'DocumentsView',
-)
-const CalendarView = lazy(() => import('@/views/calendarView'), 'CalendarView')
-const BookmarkView = lazy(
-  () => import('@/views/bookmarksView.tsx'),
-  'BookmarksView',
-)
-const InboxView = lazy(() => import('@/views/inboxView'), 'InboxView')
+import { useAppRoutes } from '@/hooks/useAppRoutes.tsx'
 
 export const AppRoutes = observer(() => {
-  const Layout = <SiteLayout />
+  const appRoutes = useAppRoutes()
+
   return (
     <>
       <RegisterGlobalHotkeys />
-      <Suspense fallback={Layout}>
-        <Routes>
-          <Route path={'/'} element={Layout}>
-            <Route index element={null} />
-            <Route path={'home'} element={<HomeView />} />
-            <Route path={'flows'} element={<FlowsView />} />
-            <Route path={'documents'} element={<DocumentsView />} />
-            <Route path={'calendar'} element={<CalendarView />} />
-            <Route path={'bookmarks'} element={<BookmarkView />} />
-            <Route path={'inbox'} element={<InboxView />} />
-            <Route path={'flows/:flowId'} element={<FlowDetailView />} />
-            <Route path={'nodes/:nodeId'} element={<NodeDetailView />} />
-          </Route>
-        </Routes>
-      </Suspense>
+      <Suspense fallback={<SiteLayout />}>{appRoutes}</Suspense>
     </>
   )
 })
---
+++ src/hooks/useAppRoutes.tsx
@@ -0,0 +1,58 @@
+import type { ComponentType } from 'react'
+
+import { lazy as _lazy } from 'react'
+import { useRoutes } from 'react-router-dom'
+
+import { SiteLayout } from '@/components/site/siteLayout.tsx'
+
+type LazyComponentModule = { [key: string]: ComponentType<any> }
+
+function lazy<T extends LazyComponentModule, K extends keyof T>(
+  importFunc: () => Promise<T>,
+  componentName: K,
+) {
+  return _lazy(() =>
+    importFunc().then((module) => ({
+      default: module[componentName],
+    })),
+  )
+}
+
+export const appLazyLoad = {
+  BookmarkView: lazy(
+    () => import('@/views/bookmarksView.tsx'),
+    'BookmarksView',
+  ),
+  CalendarView: lazy(() => import('@/views/calendarView'), 'CalendarView'),
+  DocumentsView: lazy(() => import('@/views/documentsView'), 'DocumentsView'),
+  FlowDetailView: lazy(
+    () => import('@/views/flow-detail-view'),
+    'FlowDetailViewParamsWrap',
+  ),
+  FlowsView: lazy(() => import('@/views/flowsView'), 'FlowsView'),
+  HomeView: lazy(() => import('@/views/homeView'), 'HomeView'),
+  InboxView: lazy(() => import('@/views/inboxView'), 'InboxView'),
+  NodeDetailView: lazy(
+    () => import('@/views/node-detail-view'),
+    'NodeDetailViewParamsWrap',
+  ),
+}
+export const useAppRoutes = () => {
+  return useRoutes([
+    {
+      children: [
+        { element: null, index: true },
+        { element: <appLazyLoad.HomeView />, path: 'home' },
+        { element: <appLazyLoad.FlowsView />, path: 'flows' },
+        { element: <appLazyLoad.DocumentsView />, path: 'documents' },
+        { element: <appLazyLoad.CalendarView />, path: 'calendar' },
+        { element: <appLazyLoad.BookmarkView />, path: 'bookmarks' },
+        { element: <appLazyLoad.InboxView />, path: 'inbox' },
+        { element: <appLazyLoad.FlowDetailView />, path: 'flows/:flowId' },
+        { element: <appLazyLoad.NodeDetailView />, path: 'nodes/:nodeId' },
+      ],
+      element: <SiteLayout />,
+      path: '/',
+    },
+  ])
+}
