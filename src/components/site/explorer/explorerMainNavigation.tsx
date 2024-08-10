c8cf633ef93b1c3c580e8c39baeccdabc35e8ffe
JiHoon7773 <thdwlgnsl@naver.com>
2024-08-10 11:04:54 +0900

    explorerMainNavigation

--- src/components/site/explorer/explorerMainNavigation.tsx
+++ src/components/site/explorer/explorerMainNavigation.tsx
@@ -1,3 +1,5 @@
+import type { ReactNode } from 'react'
+
 import {
   Bookmark,
   CalendarDays,
@@ -13,95 +15,76 @@ import { appPathnames } from '@/hooks/useAppRoutes.tsx'
 import { cn } from '@/utils/cn.ts'
 
 const cns = {
-  active: cn('bg-accent text-accent-foreground'),
   icon: cn('h-4 w-4 shrink-0'),
-  wrap: cn('flex justify-start gap-2'),
 }
 
 export const ExplorerMainNavigation = () => {
   const navigate = useNavigate()
   const location = useLocation()
 
+  const mainNavigations: {
+    icon: ReactNode
+    pathname: string
+    text: string
+    tooltipContent: string
+  }[] = [
+    {
+      icon: <House className={cns.icon} />,
+      pathname: appPathnames.home,
+      text: '홈',
+      tooltipContent: '홈으로 가기',
+    },
+    {
+      icon: <Map className={cns.icon} />,
+      pathname: appPathnames.flows,
+      text: '플로우 전체 보기',
+      tooltipContent: '플로우 전체 보기로 가기',
+    },
+    {
+      icon: <FileText className={cns.icon} />,
+      pathname: appPathnames.documents,
+      text: '문서 전체 보기',
+      tooltipContent: '문서 전체 보기로 가기',
+    },
+    {
+      icon: <CalendarDays className={cns.icon} />,
+      pathname: appPathnames.calendar,
+      text: '달력',
+      tooltipContent: '달력으로 가기',
+    },
+    {
+      icon: <Bookmark className={cns.icon} />,
+      pathname: appPathnames.bookmarks,
+      text: '즐겨찾기',
+      tooltipContent: '즐겨찾기로 가기',
+    },
+    {
+      icon: <Inbox className={cns.icon} />,
+      pathname: appPathnames.inbox,
+      text: '인박스',
+      tooltipContent: '인박스로 가기',
+    },
+  ]
+
   return (
     <section className={'flex w-full flex-col'}>
-      <ButtonWithTooltip
-        variant={'ghost'}
-        side={'bottom'}
-        tooltipContent={'Home 으로 가기'}
-        onClick={() => navigate(appPathnames.home)}
-        className={cn(
-          cns.wrap,
-          location.pathname === appPathnames.home && cns.active,
-        )}
-      >
-        <House className={cns.icon} />
-        <span>홈</span>
-      </ButtonWithTooltip>
-      <ButtonWithTooltip
-        variant={'ghost'}
-        side={'bottom'}
-        tooltipContent={'플로우 전체 보기로 가기'}
-        onClick={() => navigate(appPathnames.flows)}
-        className={cn(
-          cns.wrap,
-          location.pathname === appPathnames.flows && cns.active,
-        )}
-      >
-        <Map className={cns.icon} />
-        <span>플로우 전체 보기</span>
-      </ButtonWithTooltip>
-      <ButtonWithTooltip
-        variant={'ghost'}
-        side={'bottom'}
-        tooltipContent={'문서 전체 보기로 가기'}
-        onClick={() => navigate(appPathnames.documents)}
-        className={cn(
-          cns.wrap,
-          location.pathname === appPathnames.documents && cns.active,
-        )}
-      >
-        <FileText className={cns.icon} />
-        <span>문서 전체 보기</span>
-      </ButtonWithTooltip>
-      <ButtonWithTooltip
-        variant={'ghost'}
-        side={'bottom'}
-        tooltipContent={'달력으로 가기'}
-        onClick={() => navigate(appPathnames.calendar)}
-        className={cn(
-          cns.wrap,
-          location.pathname === appPathnames.calendar && cns.active,
-        )}
-      >
-        <CalendarDays className={cns.icon} />
-        <span>달력</span>
-      </ButtonWithTooltip>
-      <ButtonWithTooltip
-        variant={'ghost'}
-        side={'bottom'}
-        tooltipContent={'즐겨찾기로 가기'}
-        onClick={() => navigate(appPathnames.bookmarks)}
-        className={cn(
-          cns.wrap,
-          location.pathname === appPathnames.bookmarks && cns.active,
-        )}
-      >
-        <Bookmark className={cns.icon} />
-        <span>즐겨찾기</span>
-      </ButtonWithTooltip>
-      <ButtonWithTooltip
-        variant={'ghost'}
-        side={'bottom'}
-        tooltipContent={'인박스로 가기'}
-        onClick={() => navigate(appPathnames.inbox)}
-        className={cn(
-          cns.wrap,
-          location.pathname === appPathnames.inbox && cns.active,
-        )}
-      >
-        <Inbox className={cns.icon} />
-        <span>Inbox</span>
-      </ButtonWithTooltip>
+      {mainNavigations.map((navigation) => (
+        <ButtonWithTooltip
+          key={navigation.pathname}
+          variant={'ghost'}
+          side={'bottom'}
+          tooltipContent={navigation.tooltipContent}
+          onClick={() => navigate(navigation.pathname)}
+          className={cn(
+            'flex justify-start gap-2',
+            location.pathname === navigation.pathname &&
+              'bg-accent text-accent-foreground',
+          )}
+        >
+          {navigation.icon}
+          <span>{navigation.text}</span>
+        </ButtonWithTooltip>
+      ))}
     </section>
   )
 }
