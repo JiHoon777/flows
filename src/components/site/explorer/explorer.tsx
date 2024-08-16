c67077f17634f1c79b53525c449f7202aeba1ab6
JiHoon7773 <thdwlgnsl@naver.com>
2024-08-17 08:53:48 +0900

    루트에 플로우 생성

--- src/components/site/explorer/explorer.tsx
+++ src/components/site/explorer/explorer.tsx
@@ -5,6 +5,8 @@ import { ExplorerNodeRow } from '@/components/site/explorer/explorer-node-row.ts
 import { ExplorerMainNavigation } from '@/components/site/explorer/explorerMainNavigation.tsx'
 import { DoFlow } from '@/store/flow/do-flow.ts'
 import { useStore } from '@/store/useStore.ts'
+import { Plus, PlusCircle } from 'lucide-react'
+import { Button } from '@/components/ui/button.tsx'
 
 export const Explorer = observer(() => {
   const store = useStore()
@@ -14,7 +16,18 @@ export const Explorer = observer(() => {
     <div className={'flex w-full flex-col gap-4 px-2 pt-4'}>
       <ExplorerMainNavigation />
       <section className={'flex w-full flex-col gap-1.5'}>
-        <div className={'pl-2 text-sm'}>플로우</div>
+        <div className={'flex items-center justify-between pl-2 text-sm'}>
+          <span>플로우</span>
+          <Button
+            size={'xs'}
+            variant={'ghost'}
+            onClick={() => store.explorerView.createFlowOnRoot()}
+          >
+            <PlusCircle
+              className={'h-[18px] w-[18px] text-gray-500 hover:text-gray-900'}
+            />
+          </Button>
+        </div>
         {explorerList.map((flow) =>
           flow instanceof DoFlow ? (
             <ExplorerFlowRow key={flow.id} flow={flow} />
